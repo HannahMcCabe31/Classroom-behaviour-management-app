fce93116f3ff6f4bf3a393625d26a8a51cd55820
HannahMcCabe31 <hmccabe.hm@gmail.com>
2023-11-16 10:35:35 +0000

    adding egg image

--- src/EggItem.js
+++ src/EggItem.js
@@ -1,10 +1,15 @@
+import Egghatch1 from "./Images/Egghatch1.png"
+
 export default function EggItem(props) {
   const eggItemIndividual = props.eggItem;
   const deleteEggItemObject = props.deleteEggItemObject;
   console.log(eggItemIndividual);
   return (
     <div className="eggItem">
-      <button className="crossButton" onClick={deleteEggItemObject}>X</button>
+      <button className="crossButton" onClick={deleteEggItemObject}>
+        X
+      </button>
+      <img src={Egghatch1} className="egg-item-logo" alt="egg"/>
       <h1>{eggItemIndividual.name}</h1>
     </div>
   );
